ec2b4d2d0b8b2f9c8839247340c4bf1950e287ca
msheepy <monelson@ualberta.ca>
2025-09-17 08:38:36 -0600

    App works but unedited fields are blank

--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,36 +1,77 @@
 package com.example.listycitylab3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener, EditCityFragment.EditCityDialogListener {
 
-    private ArrayList<String> dataList;
+    private ArrayList<City> dataList;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
+    private ArrayAdapter<City> cityAdapter;
+    int curCityInd;
+
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void editCity(City city){
+        City cur = cityAdapter.getItem(curCityInd);
+        if(cur.getProvince() != ""){
+            cur.setProvince(city.getProvince());
+        }
+        if(cur.getName() != "") {
+            cur.setName(city.getName());
+        }
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public City getCity(){
+        return dataList.get(curCityInd);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
-        };
+        String[] cities = {"Edmonton", "Vancouver", "Toronto"};
+        String[] provinces = {"AB", "BC", "ON"};
 
-        dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
+        dataList = new ArrayList<City>();
+        for(int i = 0; i < cities.length; i++){
+            dataList.add(new City(cities[i], provinces[i]));
+        }
         
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new CityArrayAdapter(this, dataList);
         cityList.setAdapter(cityAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AddCityFragment().show(getSupportFragmentManager(), "Add city");
+            }
+        });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                curCityInd = i;
+                new EditCityFragment().show(getSupportFragmentManager(), "Edit city");
+            }
+        });
     }
 }
